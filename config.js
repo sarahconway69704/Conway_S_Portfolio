d7fbea84e52a6514138878c9fa92298bc688ae83
Sarah Conway <sarahconway@Sarahs-MacBook-Pro.local>
2019-12-07 23:07:49 -0500

    sigh

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-//const config = {
-   //host: "localhost",
-   // port: 8889,
-    //uname: "root",
-    //pword: "root",
-    //dbase: "portfolioData"
-//}
-
 const config = {
-     host: "us-cdbr-iron-east-05.cleardb.net",
-     uname: "b590fe05eb511",
-     pword: "6be5fb34",
-     dbase: "heroku_11f036cb4695ac2"
- }
+   host: "localhost",
+   port: 8889,
+    uname: "root",
+    pword: "root",
+    dbase: "portfolioData"
+}
+
+//const config = {
+     //host: "us-cdbr-iron-east-05.cleardb.net",
+     //uname: "b590fe05eb511",
+     //pword: "6be5fb34",
+     //dbase: "heroku_11f036cb4695ac2"
+ //}
 
 
 
