c3f8136e3ea468d2e6ba96016aa7c3fb8e448ec7
Sarah Conway <sarahconway@Sarahs-MacBook-Pro.local>
2019-12-12 22:01:35 -0500

    so helpless

--- routes/index.js
+++ routes/index.js
@@ -63,8 +63,8 @@ connect.getConnection((err, connection) => {
 		console.log(result); // this should be your database query result
 
 		// render our page
-		res.render('/', {art: result });
-		res.render('index'); // whatever page and data you're rendering
+		res.render('index', {art: result });
+		 // whatever page and data you're rendering
 	});
 });
 
