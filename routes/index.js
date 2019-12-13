05752597b4688c3d490e9351278d910a025631b5
Sarah Conway <sarahconway@Sarahs-MacBook-Pro.local>
2019-12-13 17:20:14 -0500

    miracle plz

--- routes/index.js
+++ routes/index.js
@@ -95,7 +95,7 @@ router.get('/portfolioData/:target', (req, res) => {
 	
 			// render our page
 			res.json(result[0]);
-			res.render('index', {data: targetEl});
+			res.render('index', {data: result});
 			 // whatever page and data you're rendering
 		})
 	})
