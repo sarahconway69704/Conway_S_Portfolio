f81f5494b20af4d864c74899d0550ac80063a9e1
Sarah Conway <sarahconway@Sarahs-MacBook-Pro.local>
2019-12-13 16:32:07 -0500

    jjjjjjj

--- routes/index.js
+++ routes/index.js
@@ -94,7 +94,7 @@ router.get('/portfolioData/:target', (req, res) => {
 			console.log(result); // this should be your database query result
 	
 			// render our page
-			res.render('/', {tbl_work});
+			res.render('/', {tbl_work: result});
 			 // whatever page and data you're rendering
 		})
 	})
