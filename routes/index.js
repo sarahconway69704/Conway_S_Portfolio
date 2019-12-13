6ed29c13a0361906a5a7de6e25f8bfa81c14f2c9
Sarah Conway <sarahconway@Sarahs-MacBook-Pro.local>
2019-12-13 15:47:37 -0500

    trying something else

--- routes/index.js
+++ routes/index.js
@@ -94,7 +94,7 @@ router.get('/portfolioData/:target', (req, res) => {
 			console.log(result); // this should be your database query result
 	
 			// render our page
-			res.render('home', {art: result });
+			res.render('home', {fetchData: result });
 			 // whatever page and data you're rendering
 		})
 	})
